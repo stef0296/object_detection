99f12a79b6e5bd7a01cfb6fb25147ca6fa0a755c
stef0296 <steff.alvares@gmail.com>
2021-07-18 09:59:12 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import time
+import cv2
+import numpy as np
+
+thres = 0.45 # Threshold to detect object
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 320)
+cap.set(cv2.CAP_PROP_FPS, 16)
+
+# allow the camera to warmup
+time.sleep(0.001)
+
+classNames = []
+
+classFile = 'coco.names'
+with open(classFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
+
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+while(True):
+    _, img = cap.read()
+    classIds, confs, bbox = net.detect(img,confThreshold=thres)
+    frame = np.asarray(img)
+
+    if len(classIds) != 0:
+        for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+            cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+            cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
+            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+            cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+
+    # Display
+    cv2.imshow('img', img)
+    if(cv2.waitKey(1) & 0xff == ord('q')):
+        break
+
+cv2.destroyAllWindows()
